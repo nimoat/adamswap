ad33d685d7cd5687ec3408ac3ce1bcbf8762e323
nimoat <946136985@qq.com>
2023-10-13 12:55:28 +0800

    feat: 使用action部署

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: 'export', // static导出
-	images:{
-		unoptimized: true // static导出
-	},
-	basePath: '/adamswap', // 适配 github page
+	// output: 'export', // static导出
+	// images:{
+	// 	unoptimized: true // static导出
+	// },
+	// basePath: '/adamswap', // 适配 github page
 	
 	reactStrictMode: true,
 };
